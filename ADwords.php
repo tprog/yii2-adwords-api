93e1b79651f19d0a10225ea5d519920bc90f8754
Artem Bondarenko <a.bondarenko@cronagency.com>
2015-09-16 11:30:27 +0300

    no message

--- ADwords.php
+++ ADwords.php
@@ -12,4 +12,19 @@ class ADwords extends Component
     public $developerToken = "INSERT_DEVELOPER_TOKEN_HERE";
 
     public $server_version = null;
+
+
+    private $_user = null;
+
+    public function setClient($params){
+        $this->_user = new \AdWordsUser();
+        $this->_user->LogAll();
+        $this->_user->SetClientCustomerId('889-324-3561');
+
+        $this->_user->SetOAuth2Info($oauth2Info);
+        $this->_user->SetUserAgent($this->userAgent);
+        $this->_user->SetClientLibraryUserAgent($this->userAgent);
+        $this->_user->SetClientCustomerId($clientCustomerId);
+        $this->_user->SetDeveloperToken($this->developerToken);
+    }
 }
