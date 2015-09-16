4873d9a2309ea082879c8e80229822186a9000c6
Artem Bondarenko <a.bondarenko@cronagency.com>
2015-09-16 11:14:50 +0300

    no message

--- ADwords.php
+++ ADwords.php
@@ -5,6 +5,11 @@ namespace tprog\adwordsapi;
 use yii\base\Component;
 
 
-class ADwords extends Component {
+class ADwords extends Component
+{
+    public $userAgent = "INSERT_COMPANY_NAME_HERE";
+    public $refresh_token = "INSERT_OAUTH2_REFRESH_TOKEN_HERE";
+    public $developerToken = "INSERT_DEVELOPER_TOKEN_HERE";
 
+    public $server_version = null;
 }
